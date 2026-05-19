cbdad52d942ab8e0fe3e391811833138eaf9a939
KanizSanjila <kanizsm13@gmail.com>
2026-05-20 01:08:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
+const port = process.env.PORT || 8080
+dotenv.config();
+const app = express()
+app.use(cors());
+
+
+const uri =process.env.MONGODB_URL;
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const logger = (req,res,next)=>{
+           console.log(`${req.method} | ${req.url}`);
+          next();
+       };
+
+const verifyToken = async (req, res, next) =>{
+   const { authorization } = req.headers;
+  // console.log(req.headers,"from token");
+   const token = authorization?.split(' ')[1];
+    // console.log(token);
+
+      if (!token) {
+    return res.status(401).json({ message: 'Unauthorize' });
+  }
+ 
+  try {
+    const JWKS = createRemoteJWKSet(new URL('http://localhost:3000/api/auth/jwks'));
+    const { payload } = await jwtVerify(token, JWKS);
+    // console.log(payload)
+    req.user = payload;
+
+    next();
+  } catch (error) {
+    console.error('Token validation failed:', error);
+    return res.status(401).json({ message: 'Unauthorize' });
+  }
+};       
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+        const db = client.db('databasedb');
+    const coursesCollection = db.collection('collection');
+
+       app.get('/course', async (req, res) => {
+        // console.log(req.query)
+         const { search } = req.query;
+         
+         let cursor;
+         if(search){
+           cursor = coursesCollection.find({title:{$eq:"BuildStream: Real-time Construction Analytics"}}).toArray()
+           res.send({})
+         }
+         else{
+           const cursor = coursesCollection.find();
+         }
+        const result =await cursor.toArray();
+        console.log(result)
+        res.send(result)
+    });
+
+       app.get('/featured', async (req, res) => {
+      const cursor = coursesCollection.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+       app.get('/course/:dataId',logger,verifyToken,async (req, res) => {
+        const {dataId} = req.params
+        // console.log(dataId)
+           console.log(req.user, 'req');
+        const query = {_id: new ObjectId(dataId)}
+        const result = await coursesCollection.findOne(query)
+        res.send(result)
+    });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello kaniz sanjila')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
